26756d10911020667d9d4f7f54324c522e33afe4
Brandon Kim <KimBL20@pwcs-edu.org>
2019-05-21 12:02:41 -0400

    did it

--- Desktop/Git Task 2/src/GitTask2.java
+++ Desktop/Git Task 2/src/GitTask2.java
@@ -3,9 +3,13 @@ public class GitTask2
 {
 	public static void main( String args )
 	{
-		Scanner scan = new Scanner( System.out.in );
+		Scanner scan = new Scanner( System.in );
 		StringBuilder stringy = new StringBuilder( scan.nextLine() );
 		stringy = stringy.reverse();
-		System.out.print( stringy.toString );
+		System.out.print( stringy.toString() );
+		scan.close();
 	}
 }
+// wrote toString instead of toString()
+// wrote System.out.in instead of System.in
+// forgot scan.close();
